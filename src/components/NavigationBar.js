2cf1126230e2e458504ab1079b21899c4049df66
alextheodore <alex.theodore26@gmail.com>
2024-02-06 17:35:51 +0700

    rev1

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,22 @@
+import {Navbar, Container, Nav} from "react-bootstrap"
+
+const NavigationBar = () => {
+  return (
+    <div>
+        {/* <Navbar>
+            <Container>
+                <Navbar.Brand></Navbar.Brand>
+                <Nav>
+                    <Nav.Link>POPULAR</Nav.Link>
+                    <Nav.Link>Superhero</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar> */}
+        <h1>
+            Testing
+        </h1>
+    </div>
+    )
+}
+
+export default NavigationBar
